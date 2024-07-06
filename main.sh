c01933c7db545c940c056bf705606aa5e04410cc
Joshua Nussbaum <67082011+joshuanussbaum@users.noreply.github.com>
2024-07-06 17:20:54 -0500

    update main with generator

--- main.sh
+++ main.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 python3 src/main.py
+cd publix && python3 -m http.server 8888
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from os import path, listdir, mkdir
 from shutil import copy, rmtree
+from markdown_blocks import markdown_to_html_node
 
 
 def copy_files(source: str, destination: str) -> None:
@@ -18,12 +19,34 @@ def copy_files(source: str, destination: str) -> None:
             copy_files(source_subdirectory, subdirectory)
 
 
+def extract_title(markdown: str) -> str:
+    for line in markdown.split("\n"):
+        if line.startswith("# "):
+            return line[2:].strip()
+    raise Exception("No title")
+
+
+def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
+    print(f"Generating page from {from_path} to {dest_path}, using {template_path}")
+    with open(from_path, "r") as file:
+        markdown = file.read()
+    with open(template_path, "r") as file:
+        template = file.read()
+    title = extract_title(markdown)
+    content = markdown_to_html_node(markdown).to_html()
+    template.replace("{{ Title }}", title).replace("{{ Content }}", content)
+    with open(dest_path, "w") as file:
+        file.write(template)
+
+
 def main() -> None:
     print(listdir("public"))
     print("deleting the above files and directories")
     rmtree("public")
     mkdir("public")
     copy_files("static", "public")
+    print("generating pages")
+    generate_page("content/index.md", "template.html", "public/index.html")
 
 
 if __name__ == "__main__":
